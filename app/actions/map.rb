303a3911299fe143addbf0a75530984a34f394fc
jhenderiks <justin@henderiks.ca>
2015-10-15 21:51:10 +0000

    Fixed login in query function

--- app/actions/map.rb
+++ app/actions/map.rb
@@ -1,6 +1,5 @@
 # Map page
 get '/restaumap' do
-  # binding.pry
   thing = Restaurant.where("latitude BETWEEN #{params[:lat1]} AND #{params[:lat2]} AND longitude BETWEEN #{params[:lon2]} AND #{params[:lon1]}").map do |restaurant|
     {
       name: restaurant.name,
@@ -10,8 +9,4 @@ get '/restaumap' do
       longitude: restaurant.longitude
     }
   end.to_json
-
-  # binding.pry
-
-  return thing
 end
