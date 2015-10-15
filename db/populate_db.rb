8001fdfa796574f57411390c1e2ad703db966594
jhenderiks <justin@henderiks.ca>
2015-10-15 17:30:53 +0000

    Added populate_db script (needs db/zomato_key.rb) Added zomato_key.rb to .gitignore

---
+++ db/populate_db.rb
@@ -0,0 +1,32 @@
+require 'open-uri'
+require_relative '../config/environment'
+require_relative 'zomato_key'
+
+result_count = 0
+
+restaurants = []
+
+loop do
+  api_query = open(
+    "https://developers.zomato.com/api/v2.1/search?entity_id=89&entity_type=city&start=#{result_count}&count=20",
+    "Accept" => "application/json",
+    "user_key" => ZOMATO_KEY
+  )
+
+  result_string = ""
+
+  api_query.each_line { |json| result_string += json }
+
+  result_hash = JSON(result_string)
+
+  restaurants += result_hash["restaurants"]
+
+  result_count += 20
+  puts "#{[result_count, result_hash['results_found']].min} / #{result_hash['results_found']} results parsed" if result_count % 100 == 0 || result_count >= result_hash['results_found']
+  break if result_count >= result_hash['results_found']
+end
+
+restaurants.each do |restaurant|
+  restaurant = restaurant['restaurant']
+  Restaurant.create(name: restaurant['name'], zomato_id: restaurant['id'], address: restaurant['location']['address'], locality: restaurant['location']['locality'], latitude: restaurant['location']['latitude'], longitude: restaurant['location']['longitude'], zomato_rating: restaurant['user_rating']['aggregate_rating'])
+end
