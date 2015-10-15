a90a7d5a185cd21bb722407ee8722c95c46ef6ad
jhenderiks <justin@henderiks.ca>
2015-10-15 20:30:25 +0000

    Created action to return restaurants in range of specified coordinates

--- app/actions/map.rb
+++ app/actions/map.rb
@@ -1,4 +1,12 @@
 # Map page
 get '/restaumap' do
-  return 'it works'
+  thing = Restaurant.where("latitude BETWEEN #{params[:lat1]} AND #{params[:lat2]} AND longitude BETWEEN #{params[:lon1]} AND #{params[:lon2]}").map do |restaurant|
+    {
+      name: restaurant.name,
+      address: restaurant.address,
+      zomato_rating: restaurant.zomato_rating,
+      latitude: restaurant.latitude,
+      longitude: restaurant.longitude
+    }
+  end.to_json
 end
