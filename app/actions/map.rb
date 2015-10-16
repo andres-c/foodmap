226724269589576e42d3b7d38a392e8f6af979a7
MattMcLennan <mclennan6891@gmail.com>
2015-10-16 09:02:31 -0400

    Added info box for each marker and automatically close each info box when they click somewhere else

--- app/actions/map.rb
+++ app/actions/map.rb
@@ -2,6 +2,7 @@
 get '/restaumap' do
   thing = Restaurant.where("latitude BETWEEN #{params[:lat1]} AND #{params[:lat2]} AND longitude BETWEEN #{params[:lon2]} AND #{params[:lon1]}").map do |restaurant|
     {
+      id: restaurant.id,
       name: restaurant.name,
       address: restaurant.address,
       zomato_rating: restaurant.zomato_rating,
