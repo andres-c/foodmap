77e47af6e25d9cbcd8600daae9c4006f36a9a008
MattMcLennan <mclennan6891@gmail.com>
2015-10-16 11:09:12 -0400

    Added full info box -ready to be styled

--- app/actions/map.rb
+++ app/actions/map.rb
@@ -1,5 +1,5 @@
 # Map page
-get '/restaumap' do
+post '/restaumap' do
   thing = Restaurant.where("latitude BETWEEN #{params[:lat1]} AND #{params[:lat2]} AND longitude BETWEEN #{params[:lon2]} AND #{params[:lon1]}").map do |restaurant|
     {
       id: restaurant.id,
