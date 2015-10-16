aa25f33a9bdd6654f1f81d4ed0980ba2d8b58de8
jhenderiks <justin@henderiks.ca>
2015-10-16 16:11:06 +0000

    Missed conflict resolution

--- app/actions/home.rb
+++ app/actions/home.rb
@@ -4,14 +4,10 @@ get '/' do
 end
 
 get '/map' do
-  erb :map 
-<<<<<<< HEAD
+  erb :map
 end
 
 get '/show/:id' do
   @restaurant = Restaurant.find(params[:id])
   erb :show
 end
-=======
-end
->>>>>>> 5cac556b7602a6f0631be9ab9fa52d9dd2098318
