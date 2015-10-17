b8678b0ab13cb190ce24da8090c00eaa5149d0f9
jhenderiks <justin@henderiks.ca>
2015-10-17 21:55:14 +0000

    Refactoring (cleaning up)

--- app/actions/bcrypt.rb
+++ app/actions/bcrypt.rb
@@ -1,33 +1,28 @@
-
+#login/signup/logout actions
 post '/login' do
- user = User.find_by(
-   email: params[:email],
- )
- if user and user.authenticate(params[:password])
-   session[:user_id] = user.id
-   redirect params[:fullpath]
- else
-   redirect params[:fullpath]
- end
+  user = User.find_by(email: params[:email])
+
+  session[:user_id] = user.id if user && user.authenticate(params[:password])
+    
+  redirect params[:fullpath]
 end
 
 post '/signup' do
- @user = User.new(
-   username: params[:username],
-   email: params[:email],
-   password: params[:password],
-   password_confirmation: params[:password_confirmation],
-   isAdmin: false
- )
-  if @user.save
-    session[:user_id] = @user.id
-    redirect params[:fullpath]
-  else
-    redirect params[:fullpath]
-  end
+  @user = User.new(
+    username: params[:username],
+    email: params[:email],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation],
+    isAdmin: false
+  )
+  
+  session[:user_id] = @user.id if @user.save
+  
+  redirect params[:fullpath]
 end
 
 post "/logout" do
   session.delete(:user_id)
+  
   redirect params[:fullpath]
 end
--- app/actions/map.rb
+++ app/actions/map.rb
@@ -1,4 +1,12 @@
 # Map page
+get '/map' do
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+  end
+  
+  erb :map 
+end
+
 post '/restaumap' do
   Restaurant.where("latitude BETWEEN #{params[:lat1]} AND #{params[:lat2]} AND longitude BETWEEN #{params[:lon2]} AND #{params[:lon1]}").map do |restaurant|
     {
